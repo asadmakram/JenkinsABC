0e4c6dc2a04a6e75dbfc1d720d90a6d30b037813
Asad Akram <asad.akram@contour-software.com>
2018-11-26 20:55:58 +0500

    changed list of values

--- ContainerApp/Controllers/ValuesController.cs
+++ ContainerApp/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ContainerApp.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Asad", "Jawwad" };
         }
 
         // GET api/values/5
